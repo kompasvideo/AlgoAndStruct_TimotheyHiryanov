4668b4566781e7cfb0a2ff5e4400690632c4e614
Андрей Барышников <andrey1andrey@narod.ru>
2024-10-21 23:14:23 +0300

    5_sort

---
+++ Windows/Lecture_5/CheckSort/CheckSort.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int N = 5;
+    int A[N] = { 1,2,3,4,5 };
+
+    for (int i = 0; i < N / 2; ++i) {
+        int 
+    }
+    cout << "Hello World!\n";
+}
+
---
+++ Windows/Lecture_5/Stack/Stack.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+const int MAX_A_SIZE = 100;
+
+int main()
+{
+    int N = MAX_A_SIZE;
+    int* A = new int[N];
+    int top = 0;
+    int x;
+
+    cin >> x;
+    while (x != 0) {
+        A[top++] = x;
+
+        cin >> x;
+    }
+
+    while (top > 0) {
+        cout << A[--top] << '\t';
+    }
+    cout << '\n';
+    return 0;
+}
