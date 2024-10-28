62e976b8ff163393a8bf6ae28bfe04da2078bfa9
Андрей Барышников <andrey1andrey@narod.ru>
2024-10-28 23:09:44 +0300

    checkSort

--- Windows/Lecture_5/CheckSort/CheckSort.cpp
+++ Windows/Lecture_5/CheckSort/CheckSort.cpp
@@ -5,11 +5,21 @@ using namespace std;
 int main()
 {
     const int N = 5;
-    int A[N] = { 1,2,3,4,5 };
+    int A[N] = { 5,3,2,1,4 };
 
-    for (int i = 0; i < N / 2; ++i) {
-        int 
+    int i = 0;
+    while (i < N-1) {
+        if (A[i] > A[i + 1]) {
+            int tmp = A[i];
+            A[i] = A[i+1];
+            A[i+1] = tmp;
+            i = 0;
+        }
+        else {
+            i++;
+        }
     }
-    cout << "Hello World!\n";
+    for (int i =0; i < N; ++i)
+        cout << A[i] << "\t";
 }
 
