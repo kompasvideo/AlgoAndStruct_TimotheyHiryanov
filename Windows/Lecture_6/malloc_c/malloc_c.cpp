ae8da33df4fa9f6a07ab3869fb1d3afa180f7aa6
Андрей Барышников <andrey1andrey@narod.ru>
2024-11-03 19:56:00 +0300

    lecture_6 win

---
+++ Windows/Lecture_6/malloc_c/malloc_c.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+// malloc
+int main()
+{
+    double* pd = (double*)malloc(10 * sizeof(double));
+    if (pd != NULL)
+    {
+        for (double* p = pd; p < pd + 10; p++) {
+            cout << "*p = 0.0;" << endl;
+            *p = 0.0;
+        }
+    }
+    else {
+        cout << "Не удалось выделить память";
+    }
+    free(pd);
+    cout << endl;
+}
