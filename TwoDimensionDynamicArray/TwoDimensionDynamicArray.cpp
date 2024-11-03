cb11b4df905d7cde43715ef70e8868a9ff362c74
Андрей Барышников <andrey1andrey@narod.ru>
2024-11-03 22:07:17 +0300

    TwoDimensionDynamicArray win

---
+++ TwoDimensionDynamicArray/TwoDimensionDynamicArray.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int N = 5;
+    const int M = 6;
+    int **A;
+
+
+    //for (int i = 0; i < N; i++)
+    //{
+    //    for (int j = 0; j < M; j++)
+    //    {
+    //        A[i][j] = i * M + j;
+    //    }
+    //}
+
+    //for (int i = 0; i < N; i++)
+    //{
+    //    for (int j = 0; j < M; j++)
+    //    {
+    //        cout << A[i][j] << '\t';
+    //    }
+    //    cout << '\n';
+    //}
+
+    //int* p = &A[0][0];
+    //int* p1 = reinterpret_cast<int*>(A);
+    //for (int i = 0; i < M * N; i++)
+    //{
+    //    cout << *(p1 + i) << ' ';
+    //}
+    //cout << '\n';
+}
