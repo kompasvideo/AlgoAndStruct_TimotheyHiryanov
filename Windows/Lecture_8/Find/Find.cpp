70417f9dec031806468a9c5889b3d242ce2c28a1
Андрей Барышников <andrey1andrey@narod.ru>
2024-11-04 19:45:04 +0300

    insertSort win

---
+++ Windows/Lecture_8/Find/Find.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int find(int A[], int N, int x)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int A[] = { 5,2,3,4,6,2,3 };
+    int res = find(A, 7, 4);
+    cout << res << endl;
+}
---
+++ Windows/Lecture_8/InsertSort/InsertSort.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+void insert_sort(int A[], int N)
+{
+
+}
+
+int main()
+{
+    int A[] = { 7,5,1,4,2,3,9,8,0,6 };
+    int N = 10;
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << '\t';
+    }
+    cout << endl;
+    cout << "InsertSort" << endl;
+    insert_sort(A, N);
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << '\t';
+    }
+}
