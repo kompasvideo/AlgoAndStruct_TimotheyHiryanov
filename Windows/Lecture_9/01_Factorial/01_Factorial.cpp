0310d4d41dd2b7695c1241df95ee5f4abcb980d2
Андрей Барышников <andrey1andrey@narod.ru>
2024-11-10 15:43:27 +0300

    euclidean win

--- Windows/Lecture_9/01_Factorial/01_Factorial.cpp
+++ Windows/Lecture_9/01_Factorial/01_Factorial.cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
 using namespace std;
 
+uint64_t factorial(uint16_t n)
+{
+    if (n == 0)
+        return 1;
+    return factorial(n - 1) * n;
+}
+
+// рекурсия
 int main()
 {
-    cout << "Hello World!\n";
+    uint16_t x;
+    cin >> x;
+    cout << factorial(x) << endl;
 }
