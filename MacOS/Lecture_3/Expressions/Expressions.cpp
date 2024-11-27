3833c0486faf9ed86544305633948ce0df3984a5
andrejbarysnikov <andrey1andrey@narod.ru>
2024-11-27 08:57:08 +0300

    Lecture_4 mac

--- MacOS/Lecture_3/Expressions/Expressions.cpp
+++ MacOS/Lecture_3/Expressions/Expressions.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-#
+#include <cmath>
+
 using namespace std;
+
+double f(double x);
 
 int main()
 {
-    cout << "hello" << endl;
+    cout << "Input number: ";
+    double x;
+    cin >> x;
+    cout << f(x) << endl;
+
+    double s = 0;
+    for(int n = 0, factor = 1; n < 10; n++)
+    {
+        s += pow(-1, n) * pow(x, 2 * n + 1) / factor;
+        factor *= 2 * n * (2 * n + 1);
+    }
+    cout << s << endl;
     return 0;
+}
+
+double f(double x)
+{
+    return 2 * sin(x);
 }
