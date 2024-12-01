97de6bb7a71802eb231315176720d9aa6a511b11
andrejbarysnikov <andrey1andrey@narod.ru>
2024-12-01 16:39:50 +0300

    lec_05 mac

--- MacOS/Lecture_4/SieveOfEratosthenes/SieveOfEratosthenes.cpp
+++ MacOS/Lecture_4/SieveOfEratosthenes/SieveOfEratosthenes.cpp
@@ -4,6 +4,25 @@ using namespace std;
 // Решето Эратосфена - поиск простых чисел до числа n
 int main()
 {
-    cout << "hello" << endl;
+    cout << "input number: ";
+    int n;
+    cin >> n;
+    bool* sieve = new bool[n + 1];
+    for (int i = 0; i <= n + 1; i++)
+        sieve[i]= true;
+    int x = 2;
+    while (x * x <= n+ 1)
+    {
+        if(sieve[x])
+            for(int i = x *2; i < n+1; i+=x)
+                sieve[i]= false;
+        x++;        
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if(sieve[i])
+            cout << i << "  ";
+    }
+    cout << endl;
     return 0;
 }
