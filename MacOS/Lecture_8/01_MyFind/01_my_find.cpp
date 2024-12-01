6730857e4a5921fcab9cf827ae474ecdbcd6e8be
andrejbarysnikov <andrey1andrey@narod.ru>
2024-12-02 00:15:51 +0300

    MyBinarySearch mac

---
+++ MacOS/Lecture_8/01_MyFind/01_my_find.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int left_bound(int A[], int N, int x)
+{
+    for(int i = 0; i < N; i++)
+    {
+        if (A[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int A[] = {5,2,3,4,6,2,3};
+    int res = left_bound(A,7,6);
+    cout << res << endl;
+    return 0;
+}
