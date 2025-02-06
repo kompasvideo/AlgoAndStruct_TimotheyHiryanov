e43b6a677bddca1b11bd1f7c15b3fc76280c1a38
Андрей Барышников <andrey1andrey@narod.ru>
2025-02-06 22:49:30 +0300

    win lec_9 04_MyBinGen

---
+++ Windows/Lecture_9/04_MyBinGen/04_MyBinGen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_BINARY_TO_GENERATE = 100;
+
+void generate_binary_numbers(int digit_left_to_generation)
+{
+
+}
+
+// рекурсивная генерация бинарных чисел
+int main()
+{
+    int n;
+    cout << "Enter bin number length: ";
+    cin >> n;
+    generate_binary_numbers(n);
+}
