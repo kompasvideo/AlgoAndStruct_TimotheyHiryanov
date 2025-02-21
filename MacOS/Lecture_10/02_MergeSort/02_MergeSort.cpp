113fe22a1e099db27071b7b85656fb468be984b2
andrejbarysnikov <andrey1andrey@narod.ru>
2025-02-21 22:38:17 +0300

    mac 10 02

---
+++ MacOS/Lecture_10/02_MergeSort/02_MergeSort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_ARRAY_SIZE = 100000;
+
+void input_array(double *A, int16_t n)
+{
+    for(int i =0; i < n; i++)
+        cin >> A[i];
+}
+
+void merge_sort(double *A, int16_t n)
+{
+    if (n <= 1) return;
+    int16_t middle = n / 2;
+    double *left = A;
+    double *right = A + middle;
+    int16_t left_size = middle;
+    int16_t right_size = n - middle;
+    merge_sort(left, left_size);
+    merge_sort(right, right_size);
+    double* buffer = new double[n];
+    int16_t left_i =0, right_i = 0, buffer_i =0;
+    while (left_i < left_size and right_i < right_size)
+    {
+        if(left[left_i] <= right[right_i])
+        {
+            
+        }
+    }
+}
+
+void print_array(double *A, int16_t n)
+{
+    for(int i =0; i < n; i++)
+        cout << A[i] << " ";
+    cout << endl;    
+}
+
+// сортировка слиянием
+int main()
+{
+    int16_t n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    if (n < 0 || n > MAX_ARRAY_SIZE) {
+        cerr << "Your array size is unacceptable" << endl;
+        return 1;
+    }
+    double *A = new double[n];
+    input_array(A, n);
+    merge_sort(A, n);
+    print_array(A, n);
+    delete[] A;
+    return 0;
+}
---
+++ MacOS/Lecture_9/04_MyBinGen/04_MyBinGen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_BINARY_TO_GENERATE = 100;
+
+void generate_binary_numbers(int digit_left_to_generation)
+{
+
+}
+
+int main()
+{
+    int n;
+    cout << "Enter bin number lenght: ";
+    cin >> n;
+    generate_binary_numbers(n);
+    return 0;
+}
