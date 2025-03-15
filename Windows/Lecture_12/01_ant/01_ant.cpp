46bb029bc65aa0e35eb9c780731dec903d80e381
Андрей Барышников <andrey1andrey@narod.ru>
2025-03-16 00:33:44 +0300

    win 12 03

---
+++ Windows/Lecture_12/01_ant/01_ant.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+int ant_recursive(int i, int j)
+{
+    if (i == 1 or j == 1)
+        return 1;
+    else
+    {
+        return ant_recursive(i, j - 1) + ant_recursive(i - 1, j);
+    }
+}
+
+int ant_dynamic(int n, int m)
+{
+    int result;
+    int** K = new int*[n + 1];
+    for (int i = 0; i <= n; i++)    
+        K[i] = new int[m + 1];
+    
+    // base cases
+    for (int i = 1; i <= n; i++)
+        K[i][1] = 1;
+    for (int j = 1; j <= m; j++)
+        K[1][j] = 1;
+    
+    // recursuve cases
+    for (int i = 2; i <= n; i++) {
+        for (int j = 2; j <= m; j++)
+        {
+            K[i][j] = K[i - 1][j] + K[i][j - 1];
+        }
+    }
+    result = K[n][m];
+    for (int i = 0; i <= n; i++)
+        delete[] K[i];
+    delete[] K;
+    return result;
+}
+
+// Двумерное динамическое программирование - кузнечик ходит -> и ↓
+int main()
+{
+    int (*ant)(int, int);
+    int version = 0;
+    cout << "Which version of function to use? (0 - recursive, 1 - dynamic\n";
+    cin >> version;
+    if (version == 0)
+        ant = ant_recursive;
+    else
+        ant = ant_dynamic;
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 1; j <= 10; j++) {
+            cout << ant(i, j) << '\t';
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ Windows/Lecture_12/03_bagpack/03_bagpack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int backpack_problem()
+{
+
+}
+
+int main()
+{
+    cout << "Enter number of trasures\n";
+    int trasures_number;
+    cin >> trasures_number;
+
+}
