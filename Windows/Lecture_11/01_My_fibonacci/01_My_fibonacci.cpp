95887dfb4d625a62bce45138198d5cd317112d11
Андрей Барышников <andrey1andrey@narod.ru>
2025-03-16 12:01:39 +0300

    win 11 02 my

--- Windows/Lecture_11/01_My_fibonacci/01_My_fibonacci.cpp
+++ Windows/Lecture_11/01_My_fibonacci/01_My_fibonacci.cpp
@@ -6,24 +6,12 @@ using namespace std;
 uint64_t fib_recursive(int n)
 {
     assert(n >= 0);
-    if (n == 0 or n == 1)
-        return n;
-    return fib_recursive(n - 1) + fib_recursive(n - 2);
+    
 }
 
 uint64_t fib_dynamic(int n)
 {
-    uint64_t result;
-    uint64_t* fib = new uint64_t[n + 2];
-    fib[0] = 0;
-    fib[1] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    result = fib[n];
-    delete[] fib;
-    return result;
+
 }
 
 // кузнечик на прямой, скачет на 1 или 2 клетки
---
+++ Windows/Lecture_11/02_My_kuznechik/02_My_kuznechik.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int min_cost(int n, int price[])
+{
+   
+}
+
+// кузнечик
+int main()
+{
+    cout << "Hopper economist wants to go from point 1 to point n. Enter n:";
+    int n;
+    cin >> n;
+    if (n > 100)
+    {
+        cout << "Yor number is too big." << endl;
+        return -1;
+    }
+    int price[101];
+    cout << "Enter prices of visiting point (from point 1 to point n):";
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> price[i];
+    }
+    cout << "Min cost of archieving point n from point 1 is " << min_cost(n, price) << endl;
+    return 0;
+}
