bda42dc1733edb018a3822f6a26ad272993db813
andrejbarysnikov <andrey1andrey@narod.ru>
2025-03-17 22:00:26 +0300

    mac 11 02 my

--- MacOS/Lecture_11/01_My_fibonacci/01_My_fibonacci.cpp
+++ MacOS/Lecture_11/01_My_fibonacci/01_My_fibonacci.cpp
@@ -8,19 +8,20 @@ uint64_t fib_recursive(int n)
     assert(n >= 0);
     if (n == 0 or n == 1)
         return n;
-    return fib_recursive(n -1) + fib_recursive(n - 2);
-} 
+    return fib_recursive(n-1) + fib_recursive(n-2);    
+}
 
 uint64_t fib_dynamic(int n)
 {
     uint64_t result;
-    uint64_t* fib = new uint64_t[n + 1];
+    uint64_t *fib = new uint64_t[n + 1];
     fib[0] = 0;
-    fib[1] = 1;
-    for (int i = 2; i <= n; i++)
+    if (n > 0)
+        fib[1] = 1;
+    for(int i = 2; i <= n; i++)
     {
-        fib[i] = fib[i-1] + fib[i-2];
-    }
+        fib[i] = fib_dynamic(i-1) + fib_dynamic(n-2);
+    }    
     result = fib[n];
     delete[] fib;
     return result;
