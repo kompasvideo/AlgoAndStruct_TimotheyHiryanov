15ef0e1e24d16d7bab3cb95561de48b8713394ba
andrejbarysnikov <andrey1andrey@narod.ru>
2025-03-20 21:44:53 +0300

    mac 12 01 my

--- MacOS/Lecture_11/01_My_fibonacci/01_My_fibonacci.cpp
+++ MacOS/Lecture_11/01_My_fibonacci/01_My_fibonacci.cpp
@@ -5,26 +5,12 @@ using namespace std;
 
 uint64_t fib_recursive(int n)
 {
-    assert(n >= 0);
-    if (n == 0 or n == 1)
-        return n;
-    return fib_recursive(n-1) + fib_recursive(n-2);    
+
 }
 
 uint64_t fib_dynamic(int n)
 {
-    uint64_t result;
-    uint64_t *fib = new uint64_t[n + 1];
-    fib[0] = 0;
-    if (n > 0)
-        fib[1] = 1;
-    for(int i = 2; i <= n; i++)
-    {
-        fib[i] = fib_dynamic(i-1) + fib_dynamic(n-2);
-    }    
-    result = fib[n];
-    delete[] fib;
-    return result;
+
 }
 
 // сколько есть программ получения из
--- MacOS/Lecture_11/02_My_kuznechik/02_My_kuznechik.cpp
+++ MacOS/Lecture_11/02_My_kuznechik/02_My_kuznechik.cpp
@@ -3,29 +3,7 @@ using namespace std;
 
 int min_cost(int n, int price[])
 {
-    int cost[101];
-    // base cases
-    cost[1] = price[1];
-    cost[2] = price[1] + price[2];
 
-    for (int i = 3; i <= n; i++)
-    {
-        // recursive case
-        cost[i] = min(cost[i-1], cost[i-2]) + price[i];
-    }
-
-    cout << "Min cost path (reversed) = [" << n << ' ';
-    int current = n;
-    while(current != 1)
-    {
-        if (cost[current-1] == cost[current] - price[current])
-            current--;
-        else
-            current = current - 2;
-        cout << current << ' ';        
-    }
-    cout << "]\n";
-    return cost[n];
 }
 
 int main()
