e90fab7848ba62e03a818a72a96092f6cd6ed45b
Андрей Барышников <andrey1andrey@narod.ru>
2025-06-12 13:50:57 +0300

    1

---
+++ Windows/Lecture_19/1_My_rabin_karp/1_My_rabin_karp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Алгоритм Рабина-Карпа поиска подстроки в строке за O (N)
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    string pattern;
+    getline(cin, pattern);
+
+    // считаем все степени p
+    const int p = 237;
+    
+
+    // считаем хэши от всех префиксов строки  pattern
+   
+
+    // считаем хэш от строки S
+    
+
+    // перебираем все подстроки T длины |S| и сравниваем их
+    
+    return 0;
+}
